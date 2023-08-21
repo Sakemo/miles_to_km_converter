245194bd898714344407d92328f0a7b529292db6
sketl1~ <sketlen308@gmail.com>
2023-08-21 09:44:22 -0300

    Funcionalidade do Aplicativo

--- miles_to_km.py
+++ miles_to_km.py
@@ -2,7 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 
 def convert():
-    print(entryInt.get())
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
 
 # Windows
 windows = tk.Tk()
@@ -15,15 +17,20 @@ title_label.pack()
 
 # Input Field
 input_frame = ttk.Frame(master = windows)
-entryInt = tk.IntVar()
-entry = ttk.Entry(master = input_frame, textvariable = entryInt)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_int)
 button = ttk.Button(master = input_frame, text = 'Converter', command = convert)
 entry.pack(side = 'left', padx = 10)
 button.pack(side = 'left')
 input_frame.pack(pady = 10)
 
 # Output
-output_label = ttk.Label(master= windows, text = 'Saída', font = 'Bahnschrift 18')
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master= windows, 
+    text = 'Saída', 
+    font = 'Bahnschrift 18', 
+    textvariable = output_string)
 output_label.pack(pady = 5)
 
 # Run
