dc9ff63bd41756f7906ecee6ded76fab2b707965
King <39223901+Sakemo@users.noreply.github.com>
2023-09-24 16:38:08 -0300

    Update miles_to_km.py
    Otimização do código

--- miles_to_km.py
+++ miles_to_km.py
@@ -1,37 +1,54 @@
 import tkinter as tk
-import ttkbootstrap as ttk
+from ttkbootstrap import Style
 
-def convert():
-    mile_input = entry_int.get()
-    km_output = mile_input * 1.61
-    output_string.set(km_output)
+class MilhasParaKilometrosConverter:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Conversor de Milhas para Quilômetros')
+        self.root.geometry('300x150')
+        
+        self.style = Style(theme='darkly')
+        
+        self.create_widgets()
 
-# Windows
-windows = ttk.Window(themename = 'darkly')
-windows.title('Demo')
-windows.geometry('300x150')
+    def convert(self):
+        mile_input = self.entry_int.get()
+        km_output = mile_input * 1.61
+        self.output_string.set(f'{mile_input} milhas são {km_output:.2f} quilômetros')
 
-# Title
-title_label = ttk.Label(master = windows, text = 'Milhas para Kilometros', font = 'Bahnschrift 18')
-title_label.pack()
+    def create_widgets(self):
+        title_label = tk.Label(
+            master=self.root,
+            text='Milhas para Quilômetros',
+            font='Bahnschrift 18',
+        )
+        title_label.pack()
 
-# Input Field
-input_frame = ttk.Frame(master = windows)
-entry_int = tk.IntVar()
-entry = ttk.Entry(master = input_frame, textvariable = entry_int)
-button = ttk.Button(master = input_frame, text = 'Converter', command = convert)
-entry.pack(side = 'left', padx = 10)
-button.pack(side = 'left')
-input_frame.pack(pady = 10)
+        input_frame = tk.Frame(master=self.root)
+        self.entry_int = tk.DoubleVar()
+        entry = tk.Entry(
+            master=input_frame,
+            textvariable=self.entry_int,
+        )
+        button = tk.Button(
+            master=input_frame,
+            text='Converter',
+            command=self.convert,
+        )
+        entry.pack(side='left', padx=10)
+        button.pack(side='left')
+        input_frame.pack(pady=10)
 
-# Output
-output_string = tk.StringVar()
-output_label = ttk.Label(
-    master= windows, 
-    text = 'Saída', 
-    font = 'Bahnschrift 18', 
-    textvariable = output_string)
-output_label.pack(pady = 5)
+        self.output_string = tk.StringVar()
+        output_label = tk.Label(
+            master=self.root,
+            text='Saída',
+            font='Bahnschrift 18',
+            textvariable=self.output_string,
+        )
+        output_label.pack(pady=5)
 
-# Run
-windows.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    converter_app = MilhasParaKilometrosConverter(root)
+    root.mainloop()
